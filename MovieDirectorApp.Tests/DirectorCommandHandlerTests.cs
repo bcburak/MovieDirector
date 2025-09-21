1b5d8ee0524f40ffb67a252b909e7aab1f2f422c
Burak Celik <burak.celik@mentormate.com>
2025-09-21 20:55:36 +0300

    some endpoints covered with sample unit tests

---
+++ MovieDirectorApp.Tests/DirectorCommandHandlerTests.cs
@@ -0,0 +1,37 @@
+﻿using FluentAssertions;
+using Moq;
+using MovieDirectorApp.Application.Commands;
+using MovieDirectorApp.Application.Commands.Handlers;
+using MovieDirectorApp.Domain.Entities;
+using MovieDirectorApp.Domain.Interfaces;
+
+namespace MovieDirectorApp.Tests
+{
+    public class DirectorCommandHandlerTests
+    {
+        [Fact]
+        public async Task Handle_Should_Create_Director_And_Return_Id()
+        {
+            // Arrange
+            var mockRepo = new Mock<IDirectorRepository>();
+            mockRepo.Setup(r => r.AddAsync(It.IsAny<Director>()))
+                    .Returns(Task.CompletedTask);
+
+            var handler = new DirectorCommandHandler(mockRepo.Object);
+
+            var command = new CreateDirectorCommand
+            {
+                FirstName = "Steven",
+                SecondName = "Spielberg",
+                BirthDate = new DateTime(1946, 12, 18)
+            };
+
+            // Act
+            var result = await handler.Handle(command, CancellationToken.None);
+
+            // Assert
+            result.Should().NotBeNullOrEmpty();
+            mockRepo.Verify(r => r.AddAsync(It.IsAny<Director>()), Times.Once);
+        }
+    }
+}
---
+++ MovieDirectorApp.Tests/MovieRepositoryTests.cs
@@ -0,0 +1,35 @@
+﻿using MongoDB.Driver;
+using Moq;
+using MovieDirectorApp.Domain.Entities;
+using MovieDirectorApp.Infrastructure.Repositories;
+
+namespace MovieDirectorApp.Tests
+{
+    public class MovieRepositoryTests
+    {
+
+        [Fact]
+        public async Task AddAsync_Should_Insert_Movie()
+        {
+            // Arrange
+            var mockCollection = new Mock<IMongoCollection<Movie>>();
+            var mockDb = new Mock<IMongoDatabase>();
+            mockDb.Setup(d => d.GetCollection<Movie>("Movies", null))
+                  .Returns(mockCollection.Object);
+
+            var repo = new MovieRepository(mockDb.Object);
+            var movie = new Movie
+            {
+                Title = "Jurassic Park",
+                Year = 1993
+            };
+
+            // Act
+            await repo.AddAsync(movie);
+
+            // Assert
+            mockCollection.Verify(c => c.InsertOneAsync(
+                It.IsAny<Movie>(), null, default), Times.Once);
+        }
+    }
+}
